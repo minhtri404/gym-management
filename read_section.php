495f8454b30adb39fab587a13ab867eb287882d2
trideptrai792 <tritruongminh2020@gmail.com>
2026-07-06 19:34:27 +0700

    security: remove unsafe maintenance endpoints

--- read_section.php
+++
@@ -1,5 +0,0 @@
-<?php
-$lines = file('user/home.php');
-for ($i = 433; $i <= 455; $i++) {
-    echo ($i+1) . ': ' . $lines[$i];
-}
--- replace_gym_name.php
+++
@@ -1,5 +0,0 @@
-<?php
-$content = file_get_contents('user/home.php');
-$content = str_replace('The New Gym tặng bạn', 'FLEXZONE tặng bạn', $content);
-file_put_contents('user/home.php', $content);
-echo "Đã thay thế xong!";
